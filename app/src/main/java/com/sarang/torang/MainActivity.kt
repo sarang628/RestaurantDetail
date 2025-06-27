31fa8e2b42d8cf91ebc066190154d3f6bc90de9a
sryang <sarang628@gmail.com>
2025-06-27 16:26:13 +0800

    update

--- app/src/main/java/com/sarang/torang/MainActivity.kt
+++ app/src/main/java/com/sarang/torang/MainActivity.kt
@@ -3,8 +3,6 @@ package com.sarang.torang
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
