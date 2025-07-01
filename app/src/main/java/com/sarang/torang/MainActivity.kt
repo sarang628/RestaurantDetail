c07bcafb610632d1f02fdccdb11b97cfa3db6df9
sryang <sarang628@gmail.com>
2025-07-01 15:07:29 +0800

    update

--- app/src/main/java/com/sarang/torang/MainActivity.kt
+++ app/src/main/java/com/sarang/torang/MainActivity.kt
@@ -4,11 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.ui.Modifier
-import com.sarang.library.RestaurantDetailNavigationScreen
 import com.sarang.library.compose.DetailRestaurantInfo
 import com.sarang.library.compose.LocalDetailRestaurantInfo
 import com.sarang.library.compose.LocalImageLoader
@@ -22,24 +18,24 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TorangTheme {
                 CompositionLocalProvider(
                     LocalImageLoader provides customImageLoader,
-                    LocalDetailRestaurantInfo provides custonRestaurantInfo,
-                    com.sarang.torang.LocalImageLoader provides restaurantImageLoader
+                    LocalDetailRestaurantInfo provides customRestaurantInfo,
+                    LocalRestaurantInfoImageLoader provides restaurantImageLoader
                 ) {
-                    //RestaurantDetailScreenTest()
-                    RestaurantDetailNavigationScreen_()
+                    RestaurantDetailScreen(restaurantId = 234)
                 }
             }
         }
     }
 }
 
-val custonRestaurantInfo: DetailRestaurantInfo = {
+val customRestaurantInfo: DetailRestaurantInfo = {
     RestaurantInfoWithPermission(restaurantId = it, viewModel = BestPracticeViewModel())
 }
 
@@ -49,38 +45,3 @@ val restaurantImageLoader: RestaurantInfoImageLoader = { modifier, url, width, h
     provideTorangAsyncImage().invoke(modifier, url, width, height, scale)
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun RestaurantDetailScreenTest(restaurantId: Int = 234) {
-    RestaurantDetailScreen(restaurantId = restaurantId)
-}
-
-@Composable
-@OptIn(ExperimentalMaterial3Api::class)
-fun RestaurantDetailNavigationScreen_(restaurantId: Int = 234) {
-    RestaurantDetailNavigationScreen(
-        restaurantId = restaurantId,
-        feed = {
-            /*Feed(
-                review = Review.empty().copy(
-                    user = User.empty()
-                        .copy(name = it.name, profilePictureUrl = it.profilePictureUrl),
-                    rating = 4.0f,
-                    reviewImages = it.reviewImages,
-                    contents = it.contents,
-                    restaurant = Restaurant(it.restaurantId, it.restaurantName)
-                ),
-                imageLoadCompose = provideTorangAsyncImage1(),
-                expandableText = { modifier, a, b, c -> ExpandableText(modifier, a, b, c) })*/
-        }
-    )
-}
